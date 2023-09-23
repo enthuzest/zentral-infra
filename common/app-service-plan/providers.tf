215c763eb2ec2183139e092068b6caafcb17dff7
faraz ahmad siddiqui <faraz841@gmail.com>
2023-09-23 22:47:35 +1000

    updated code

--- common/app-service-plan/providers.tf
+++ common/app-service-plan/providers.tf
@@ -8,27 +8,28 @@ terraform {
   }
   backend "azurerm" {}
 }
+
+# provider "azurerm" {
+#   subscription_id = var.subscription_id
+#   features {}
+#   alias = "user"
+# }
+
+# data "azurerm_key_vault" "automation" {
+#   name                = "automata-kv"
+#   resource_group_name = "automata"
+#   provider            = azurerm.user
+# }
+
+# data "azurerm_key_vault_secret" "arm_secret" {
+#   name         = "arm-client-secret"
+#   key_vault_id = data.azurerm_key_vault.automation.id
+#   provider     = azurerm.user
+# }
+
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  features {}
-  alias = "user"
-}
-
-data "azurerm_key_vault" "automation" {
-  name                = "automata-kv"
-  resource_group_name = "automata"
-  provider            = azurerm.user
-}
-
-data "azurerm_key_vault_secret" "arm_secret" {
-  name         = "arm-client-secret"
-  key_vault_id = data.azurerm_key_vault.automation.id
-  provider     = azurerm.user
-}
-
-provider "azurerm" {
-  client_id       = "5103dd4a-faad-4fc7-b96b-6f10cb7bbc66"
-  client_secret   = data.azurerm_key_vault_secret.arm_secret
+  # client_id       = "5103dd4a-faad-4fc7-b96b-6f10cb7bbc66"
+  # client_secret   = data.azurerm_key_vault_secret.arm_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   features {}
