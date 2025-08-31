54ff9b7313891b2c41caa2f769cec01afa0c5d8f
faraz841 <faraz841.aus@gmail.com>
2025-08-31 18:51:31 +1000

    removed old code

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZENTRAL INFRA
+
+This repo contains the resources that are shared by application that uses zentral to create it's infrastructure
--- applications/demo-app/func.tf
+++
@@ -1,9 +0,0 @@
-module "func-app" {
-  source              = "github.com/faraz841/Zentral//modules/function-app"
-  function_app_name   = local.application_name_with_sub_environment
-  resource_group_name = azurerm_resource_group.primary_rg.name
-  app_service_plan_id = data.azurerm_app_service_plan.primary-asp.id
-  location            = azurerm_resource_group.primary_rg.location
-  app_settings        = local.app_settings
-  tags                = var.tags
-}
